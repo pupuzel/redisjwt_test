34028e07840ba1615fb5fb192c5152a5e6e0af63
pupuzel <jjdo1994@gmail.com>
2021-04-02 18:04:20 +0900

    퇴근전 마지막

--- src/test/java/com/jockjock/token/TestJPA.java
+++ src/test/java/com/jockjock/token/TestJPA.java
@@ -1,21 +1,26 @@
 package com.jockjock.token;
 
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.jockjock.token.domain.post.Post;
 import com.jockjock.token.domain.post.PostDAO;
 
 
+//JUnit4에서는 RunWith(MockitoJUnitRunner.class)를,
+//JUnit5에서는 ExtendWith를 쓰도록 되어있다
+
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+//@ActiveProfiles("test")
 class TestJPA {
 	
 	@Autowired
@@ -26,11 +31,15 @@ class TestJPA {
 	@Test
 	@Rollback(false)
 	void TestJPA() {
-		List<Post> list = postDAO.findAll();
-		list.stream().forEach( post -> {
-			System.out.println(post.getId());
-		});
-
+		
+		Post post = new Post("test","test","test");
+		postDAO.save(post);
+		
+		int updateCount = postDAO.updateContent(2L, "바뀐내용");
+		System.out.println("--------updateCount : "+updateCount+"-----------");
+		
+		String content = postDAO.findById(2L).get().getContent();
+		System.out.println("--------content : "+content+"-----------");
 	}
 
 
